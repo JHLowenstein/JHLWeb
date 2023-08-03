3bb3fac36841f81ef134b2928415f586d29bbcf7
JHLowenstein <jhlGitHub@gmail.com>
2023-08-02 22:44:51 -0400

    updated hero-home

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,5 +1,9 @@
 import HeroLogo from '@/public/images/favicon.png'
 import Image from 'next/image'
+import Hair from '@/public/images/hm.png'
+import Fur from '@/public/images/fm.png'
+import Leather from '@/public/images/lm.png'
+import Link from 'next/link'
 
 export default function HeroHome() {
   return (
@@ -45,11 +49,11 @@ export default function HeroHome() {
         {/* Hero content */}
         <section className='relative'>
           <div className='max-w-6xl mx-auto px-4 sm:px-6'>
-            <div className='pt-32 pb-12 md:pt-40 md:pb-20'>
+            <div className='pt-32 pb-12 md:pt-40 md:pb-10'>
               <div className='max-w-3xl mx-auto text-center'>
                 {/* Top image */}
                 <div
-                  className='relative inline-flex flex-col mb-6'
+                  className='relative inline-flex flex-col mb-2'
                   data-aos='fade-up'
                 >
                   <Image
@@ -79,36 +83,141 @@ export default function HeroHome() {
                   </span>{' '}
                 </h1>
                 <p
-                  className='text-xl text-gray-400 mb-8'
+                  className='text-xl text-white mb-8'
                   data-aos='fade-up'
                   data-aos-delay='200'
-                ></p>
-                <div className='max-w-xs mx-auto sm:max-w-none flex flex-col flex-row justify-center'>
+                >
+                  the new{' '}
+                  <span className='text-transparent bg-clip-text  bg-gradient-to-r from-pink-300 via-emerald-300 to-violet-300 '>
+                    YOU
+                  </span>{' '}
+                  is born in our color labs{' '}
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className='max-w-6xl mx-auto px-4 sm:px-6'>
+            <div className=''>
+              {/*  */}
+              <div className='max-w-sm mx-auto grid gap-8 lg:grid-cols-3  items-start lg:max-w-none'>
+                {/* Hair Box */}
+                <div
+                  className='flex flex-col h-full px-6 pb-6bg-white-800 rounded-xl'
+                  data-aos='fade-up'
+                >
+                  <Link href='/hair'>
+                    <Image
+                      className='rounded-lg'
+                      src={Hair}
+                      width={300}
+                      height={200}
+                      alt='Testimonial 01'
+                      style={{
+                        margin: 'auto',
+                        pointerEvents: 'none',
+                      }}
+                    />
+                  </Link>
+                </div>
+
+                {/* Fur Box */}
+                <div
+                  className='flex flex-col h-full px-6 pb-6 bg-white-800 rounded-xl'
+                  data-aos='fade-up'
+                >
+                  <Link href='/fur'>
+                    <Image
+                      className='rounded-lg'
+                      src={Fur}
+                      width={300}
+                      height={200}
+                      alt='Testimonial 01'
+                      style={{
+                        margin: 'auto',
+                        pointerEvents: 'none',
+                      }}
+                    />
+                  </Link>
+                </div>
+                {/* <div
+                  className='flex flex-col h-full p-6 bg-white-800 rounded-xl'
+                  data-aos='fade-up'
+                >
+                  <div>
+                    <div className='  mb-4'>
+                      <Image
+                        className='rounded-xl'
+                        src={Hair}
+                        width={300}
+                        height={200}
+                        alt='Testimonial 01'
+                        style={{
+                          margin: 'auto',
+                          pointerEvents: 'none',
+                        }}
+                      />
+                    </div>
+                  </div>
+
                   <div data-aos='fade-up' data-aos-delay='400'>
                     <a
-                      className='btn text-xl text-white font-semibold bg-pink-300 rounded-xl hover:bg-pink-400 w-full mb-10 sm:w-auto '
-                      href='/hair'
-                    >
-                      HAIR
-                    </a>
-                  </div>
-                  <div data-aos='fade-up' data-aos-delay='600'>
-                    <a
-                      className='btn text-white text-xl font-semibold bg-emerald-300 rounded-xl hover:bg-emerald-400 w-full sm:w-auto mb-10'
+                      className='btn text-xl text-white font-semibold bg-emerald-300 rounded-xl hover:bg-emerald-400 w-full mb-10 sm:w-auto '
                       href='/fur'
                     >
                       FUR
                     </a>
-                    <div data-aos='fade-up' data-aos-delay='600'>
-                      <a
-                        className='btn text-white text-xl font-semibold bg-violet-300 rounded-xl hover:bg-violet-400 w-full sm:w-auto sm:mb-0'
-                        href='/leather'
-                      >
-                        LEATHER
-                      </a>
+                  </div>
+                </div> */}
+                {/* Leather Box */}
+                <div
+                  className='flex flex-col h-full px-6 pb-6 bg-white-800 rounded-xl'
+                  data-aos='fade-up'
+                >
+                  <Link href='/leather'>
+                    <Image
+                      className='rounded-lg'
+                      src={Leather}
+                      width={300}
+                      height={200}
+                      alt='Testimonial 01'
+                      style={{
+                        margin: 'auto',
+                        pointerEvents: 'none',
+                      }}
+                    />
+                  </Link>
+                </div>
+                {/* <div
+                  className='flex flex-col h-full p-6 bg-white-800 rounded-xl'
+                  data-aos='fade-up'
+                >
+                  <div>
+                    <div className='  mb-4'>
+                      <Image
+                        className='rounded-xl'
+                        src={Hair}
+                        width={300}
+                        height={200}
+                        alt='Testimonial 01'
+                        style={{
+                          margin: 'auto',
+                          pointerEvents: 'none',
+                        }}
+                      />
                     </div>
                   </div>
-                </div>
+
+                  <div data-aos='fade-up' data-aos-delay='400'>
+                    <a
+                      className='btn  text-xl text-white font-semibold bg-violet-300 rounded-xl hover:bg-violet-400 w-full mb-10 sm:w-auto '
+                      href='/leather'
+                    >
+                      LEATHER
+                    </a>
+                  </div>
+                </div> */}
               </div>
             </div>
           </div>
